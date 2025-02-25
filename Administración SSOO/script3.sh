456e84f3f9d09cc901841e987db5dc53e3874f3e
Vicky <mvictoriascr@gmail.com>
2025-02-25 21:59:56 +0000

    Add files via upload

---
+++ Administración SSOO/script3.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#Realiza un **Shell Script** que solicite el **nombre del fichero** (del **Ejercicio 2**), realizando las siguientes comprobaciones:  
+#Verificar si **existe**.  
+#Comprobar si es un **fichero**.  
+#Verificar que el usuario tiene **permisos de lectura**.  
+#Leer el fichero y mostrarlo en pantalla en formato de **tabla**.  
+
+read -p "Introduce el nombre del fichero: " fichero
+
+if [ ! -e "$fichero" ]; then
+    echo "El fichero '$fichero' no existe."
+    exit 
+fi
+
+if [ ! -f "$fichero" ]; then
+    echo "'$fichero' no es un fichero."
+    exit 
+fi
+
+if [ ! -r "$fichero" ]; then
+    echo "No tienes permisos de lectura sobre '$fichero'."
+    exit 
+fi
+
+printf "| %-3s | %-10s | %-10s | %-10s |\n" "Nº" "Nombre" "Apellido 1" "Apellido 2"
+awk -F":" '{printf "| %-3s | %-10s | %-10s | %-10s |\n", $1, $2, $3, $4}' "$fichero"
+
+

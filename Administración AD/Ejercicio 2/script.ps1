0805bd7af78d620ee6c5e6b784793954b5650c53
Vicky <mvictoriascr@gmail.com>
2025-02-25 11:21:22 +0000

    Add files via upload

---
+++ Administración AD/Ejercicio 2/script.ps1
@@ -0,0 +1,7 @@
+Import-Csv -Path "C:\users\vicky\Desktop\Ejercicio 2\Jugadores.csv" | ForEach-Object {
+New-ADOrganizationalUnit -Name $_.Nombre -Path $_.ruta
+}
+
+Import-Csv -Path "C:\users\vicky\Desktop\Ejercicio 2\Ligas.csv" | ForEach-Object {
+New-ADOrganizationalUnit -Name $_.Nombre -Path $_.ruta
+}

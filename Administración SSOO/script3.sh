4f1e386971d7410760514bb0cfcc74aed4b529d8
Vicky <mvictoriascr@gmail.com>
2025-02-26 09:39:06 +0100

    Update script3.sh

--- Administración SSOO/script3.sh
+++ Administración SSOO/script3.sh
@@ -6,24 +6,22 @@
 #Verificar que el usuario tiene **permisos de lectura**.  
 #Leer el fichero y mostrarlo en pantalla en formato de **tabla**.  
 
-read -p "Introduce la ruta del fichero: " fichero
-
-if [ ! -e "$fichero" ]; then
+read -p "Introduce el nombre del fichero: " fichero
+rutafichero=$(find / -name "$fichero" -type f 2>/dev/null)
+if [ ! -e "$rutafichero" ]; then
     echo "El fichero '$fichero' no existe."
     exit 
 fi
 
-if [ ! -f "$fichero" ]; then
+if [ ! -f "$rutafichero" ]; then
     echo "'$fichero' no es un fichero."
     exit 
 fi
 
-if [ ! -r "$fichero" ]; then
-    echo "No tienes permisos de lectura en '$fichero'."
+if [ ! -r "$rutafichero" ]; then
+    echo "No tienes permisos de lectura sobre '$fichero'."
     exit 
 fi
 
 printf "| %-3s | %-10s | %-10s | %-10s |\n" "Nº" "Nombre" "Apellido 1" "Apellido 2"
-awk -F":" '{printf "| %-3s | %-10s | %-10s | %-10s |\n", $1, $2, $3, $4}' "$fichero"
-
-
+awk -F":" '{printf "| %-3s | %-10s | %-10s | %-10s |\n", $1, $2, $3, $4}' "$rutafichero"

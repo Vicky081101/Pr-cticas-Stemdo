eeb50a3ff15f371b788fbfe828011090d69d9eda
Vicky <mvictoriascr@gmail.com>
2025-03-04 19:26:34 +0000

    Add files via upload

---
+++ JavaScript/Ejercicios con operadores de comparación y lógicos/Ejercicio7.js
@@ -0,0 +1,8 @@
+//Utiliza la consola para escribir una expresión que compare dos strings y produzca un valor TRUE.
+console.log("Hola mundo" == "Hola mundo")
+//Escribí una expresión comparando dos strings que devuelva FALSE.
+console.log("Hola" == "Adios")
+//Escribí una expresión que involucre un string y un número y devuelva FALSE.
+console.log("Numero" == 3)
+//Escribí una expresión que involucre un string y un número y devuelva TRUE
+console.log("8" == 8)

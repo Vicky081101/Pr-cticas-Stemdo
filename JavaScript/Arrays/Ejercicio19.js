1b9b46a15056f1e49ddc690c3449b9d54e6e5c05
Vicky <mvictoriascr@gmail.com>
2025-03-06 11:38:18 +0100

    Update Ejercicio19.js

--- JavaScript/Arrays/Ejercicio19.js
+++ JavaScript/Arrays/Ejercicio19.js
@@ -1,3 +1,7 @@
+//Crea un array que se llame toDoList con al menos 5 tareas para hacer.
+//A través del método indexOf() encontrar el indice de una de las 5 tareas especificadas en el array.
+//Borrar el item del array toDoList, que ha sido devuelto por el método indexOf().
+
 let toDoList = ['Dormir','Comer','Jugar','Farmear','Cocinar']
 let ind = toDoList.indexOf('Cocinar')
 toDoList.splice(ind, 4)

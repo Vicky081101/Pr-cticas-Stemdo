3538abac49282e1f28673405537d73e5d079e4a2
Vicky <mvictoriascr@gmail.com>
2025-03-06 09:49:29 +0100

    Update Ejercicio18.js

--- JavaScript/Arrays/Ejercicio18.js
+++ JavaScript/Arrays/Ejercicio18.js
@@ -1,3 +1,6 @@
+//Declara un array llamado ensaladaDeFrutas.
+//Usa un bucle For y muestra cada ítem de ensaladaDeFrutas en la consola, con el siguiente mensaje: “Las frutas que hay en la ensalada son:”
+
 let ensaladaDeFrutas = ['manzana','sandia','pera','platano','fresa']
 
 for (let i=0;i<ensaladaDeFrutas.length;i++) {

a0f702cd1628d636a70ec7d5a314da2c25d3973a
Vicky <mvictoriascr@gmail.com>
2025-03-06 08:48:59 +0000

    Add files via upload

---
+++ JavaScript/Arrays/Ejercicio18.js
@@ -0,0 +1,5 @@
+let ensaladaDeFrutas = ['manzana','sandia','pera','platano','fresa']
+
+for (let i=0;i<ensaladaDeFrutas.length;i++) {
+    console.log ("Las frutas que hay en la ensalada son: " + ensaladaDeFrutas[i])
+}

e13f3683917d208249b359187aeb3e350bc8a3e2
Vicky <mvictoriascr@gmail.com>
2025-03-07 09:52:20 +0000

    Add files via upload

---
+++ JavaScript/Funciones/Ejercicio23.js
@@ -0,0 +1,7 @@
+//Declara una función llamada saludo() que muestre una alerta de Bienvenida al visitante al invocarla por la consola.
+
+function saludo () {
+    alert("Bienvenidos a mi página para probar el script")
+}
+
+saludo();

1321b3d9531f2d1f3a62794567fd4b51b79bec35
Vicky <mvictoriascr@gmail.com>
2025-03-07 09:10:17 +0100

    Update Ejercicio20.js

--- JavaScript/Arrays/Ejercicio20.js
+++ JavaScript/Arrays/Ejercicio20.js
@@ -1,3 +1,5 @@
+//Crear un array edades con al menos 8 edades distintas.
+//Recorrer el array y obtener el promedio de las edades del array.
 const edades = [9,24,70,44,35,66,2,8];
 let suma = 0;
 let numelem= edades.length;
